a4dd9ae6220020faaa349855784ae8d72d9fb2e5
RRKarthik <raviramkarthik7@gmail.com>
2021-06-26 20:22:43 +0530

    Created basic UI and functionality to retrieve weather based on places entered by user

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-console.log("Hello");
-async function api()
+const form = document.getElementById("form");
+const btn = document.getElementById("submit");
+btn.addEventListener("click",getPlace);
+function getPlace(e)
 {
-let x = await fetch("https://api.openweathermap.org/data/2.5/weather?q=visakhapatnam&appid=b79045b9e20ad59b0eb0026df9832112",{mode:'cors'});
+    e.preventDefault();
+    let place = form.elements[0].value;
+    api(place);
+}
+async function api(value)
+{
+let x = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=b79045b9e20ad59b0eb0026df9832112`,{mode:'cors'});
 console.log(await x.json());
 }
-api();
